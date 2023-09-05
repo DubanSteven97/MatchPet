58164d29d1a6bae736385c61a9d10df59e916744
JBenavidesM1 <jbenavidesm1@ucentral.edu.co>
2023-09-05 15:14:21 -0500

    Se adiciona modelo MatchPetDal

---
+++ MatchPetDal/DBMatchpet.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatchPetDal
+{
+    public partial class DBMatchpet
+    {
+        public string ConnectionString { get; set; }
+
+        public DBMatchpet(string connectionString)
+            : base(connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+        public Organizacion GetOrganizacion(int idOrganizacion)
+        {
+            try
+            {
+                var res = from o in this.Organizacion
+                          where o.idOrganizacion == idOrganizacion
+                          select o;
+                return res.First();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola");
+
+        }
+    }
+}
