aae8ce8def470a510883dd934579cb7fcbfcb102
Duban Steven Estupiñan Parra <duvan.15.97@hotmail.com>
2023-09-05 18:35:22 -0500

    Se agregan los servicios para listar un solo asociado y actualizar, tambien se ajusta en de listar todos los asociados para que solo muestre los activos y que no esten borrados

--- webapi/Controllers/OrganizacionController.cs
+++ webapi/Controllers/OrganizacionController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using System.Security.Claims;
 using webapi.Models;
 
@@ -27,7 +28,7 @@ namespace webapi.Controllers
 
         public async Task<IActionResult> Lista()
         {
-            var listaOrganizaciones = await _baseDatos.Organizacions.ToListAsync();
+            var listaOrganizaciones = await _baseDatos.Organizacions.Where(x => x.Activo == true && x.Borrado == false).ToListAsync();
             var identity = HttpContext.User.Identity as ClaimsIdentity;
             var respuestaToken = Jwt.validarToken(identity);
 
@@ -47,6 +48,39 @@ namespace webapi.Controllers
             }
         }
 
+        [HttpGet]
+        [Route("Registro/{id:int}")]
+        [Authorize]
+
+        public async Task<IActionResult> Registro(int id)
+        {
+            var organizacion = await _baseDatos.Organizacions.FirstOrDefaultAsync(o => o.IdOrganizacion == id); ;
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            var respuestaToken = Jwt.validarToken(identity);
+
+            if (!respuestaToken.success)
+            {
+                return BadRequest(respuestaToken);
+            }
+
+            if (respuestaToken.success == true)
+            {
+                if (organizacion != null)
+                {
+                    return Ok(organizacion);
+                }
+                else
+                {
+                    return BadRequest("No existe la organización");
+                }
+                
+            }
+            else
+            {
+                return BadRequest("Token invalido");
+            }
+        }
+
         [HttpPost]
         [Route("Agregar")]
         [Authorize]
@@ -58,7 +92,49 @@ namespace webapi.Controllers
             return Ok(request);
         }
 
+        [HttpPost]
+        [Route("Actualizar/{id:int}")]
+        [Authorize]
 
+        public async Task<IActionResult> Actualizar([FromBody] Object request, int id)
+        {
+            var data = JsonConvert.DeserializeObject<dynamic>(request.ToString());
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            var respuestaToken = Jwt.validarToken(identity);
+
+            if (!respuestaToken.success)
+            {
+                return BadRequest(respuestaToken);
+            }
+
+            if (respuestaToken.success == true)
+            {
+                // Busca la organización que deseas actualizar por su ID.
+                var organizacion = await _baseDatos.Organizacions.FirstOrDefaultAsync(o => o.IdOrganizacion == id);
+
+                if (organizacion == null)
+                {
+                    return BadRequest("No existe la organización");
+                }
+
+                // Aplica los cambios en la entidad con los valores del objeto "request".
+                organizacion.Nombre = data.nombre == null ? organizacion.Nombre  : data.nombre.ToString();
+                organizacion.Activo = data.activo == null ? organizacion.Activo : bool.Parse(data.activo.ToString());
+                organizacion.Borrado = data.borrado == null ? organizacion.Borrado : bool.Parse(data.borrado.ToString());
+
+    
+                // Agrega todas las propiedades que desees actualizar.
+
+                // Guarda los cambios en la base de datos.
+                await _baseDatos.SaveChangesAsync();
+
+                return Ok(organizacion);
+            }
+            else
+            {
+                return BadRequest("Token invalido");
+            }
+        }
 
         [HttpDelete]
         [Route("Eliminar/{id:int}")]
