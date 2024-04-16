7544b97a6d873928d69ea39318e4a389234b259d
Juan Sebastian Benavides <sebastianbm97@gmail.com>
2024-04-16 12:25:06 -0500

    Se agrega endpoint para consultar listado de usuarios

---
+++ MatchPetBusiness/PersonaBusiness.cs
@@ -0,0 +1,35 @@
+﻿using MatchPetDal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatchPetBusiness
+{
+    public class PersonaBusiness
+    {
+        public PersonaBusiness()
+        {
+        }
+
+        public List<Persona> getUsuarios()
+        {
+            try
+            {
+                List<Persona> result = null;
+                using (var dbContext = new DBMatchpet())
+                {
+                    var us = from u in dbContext.Persona
+                              select u;
+                    result = (List<Persona>)us.ToList();
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ webapi/Controllers/PersonaController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using webapi.Models;
+
+using MatchPetDal;
+using Microsoft.AspNetCore.Http;
+
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Data.Entity;
+using MatchPetBusiness;
+
+namespace webapi.Controllers
+{
+    [Route("Matchpet/[controller]")]
+    [ApiController]
+    public class PersonaController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("GetUsuarios")]
+
+        public string GetUsuarios()
+        {
+            PersonaBusiness usuario = new PersonaBusiness();
+
+            List<Persona> per = usuario.getUsuarios();
+
+            List<UsuarioApp> usus = new List<UsuarioApp>();
+
+            foreach (Persona persona in per)
+            {
+                UsuarioApp usu = new UsuarioApp();
+                usu.idPersona = persona.idPersona;
+                usu.nombres = persona.nombres;
+                usu.apellidos = persona.apellidos;
+                usu.telefono = persona.telefono;
+                usu.email = persona.email;
+                usu.password = persona.password;
+                usus.Add(usu);
+            }
+
+            string json = "";
+            json = JsonConvert.SerializeObject(usus);
+            return json;
+    
+        }
+    }
+}
